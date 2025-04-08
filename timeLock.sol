7d53fd818c013cf10480a49c7d9ac962258c5caf
ian <vczvip@gmail.com>
2025-04-08 18:49:59 +0800

    ok

--- timeLock.sol
+++ timeLock.sol
@@ -3,77 +3,69 @@ pragma solidity ^0.8.0;
 
 
 contract TimeLock {
-    event Queue(address indexed caller, address target,  bytes32 txId, uint256 value, bytes data, uint256 timestamp);
-    event Execute(address indexed caller, address target, bytes32 txId, uint256 value, bytes data, uint256 timestamp);
+    event Queue(address indexed caller, bytes32 txId);
+    event Exec(address indexed caller, bytes32 txId, bytes data);
     event Cancel(address indexed caller, bytes32 txId);
 
     address public owner;
     uint256 private constant delayTime = 60;
-    uint256 private constant execTime = 600;
 
+    enum TxStatus {None, Queued, Executed, Canceled} // 0未定义 1已排队 2已执行 3已取消
     struct Transaction {
+        address owner;
         address target;
         uint value;
         bytes data;
-        uint256 timestamp;
-        bool execute;
-        bool cancel;
+        uint256 scheduleTime;
+        uint256 unlockTime;
+        TxStatus status; 
     }
-    mapping(address => mapping(bytes32 => Transaction)) private callerTxIdTx;
+    mapping(bytes32 => Transaction) private txs;
     mapping(address => bytes32[]) private callerTxIds;
-    mapping(bytes32 => address) private txIdCaller;
 
     constructor() {owner = msg.sender;}
 
     receive() external payable {}
 
-    function queue(address _target, uint256 _value, bytes calldata _data) external returns(bytes32) {
+    function queue(address _target, uint256 _value, bytes calldata _data, uint256 _scheduleTime) external  {
         require(_target != address(0),"invalid target address");
+        require(_scheduleTime >= block.timestamp, "schedule time must be in future");
 
-        uint256 blockTimestamp = block.timestamp;
-        bytes32 txId = _getHash(_target, _value, _data, blockTimestamp);
-        require(callerTxIdTx[msg.sender][txId].target == address(0),"tx already exist");
+        bytes32 txId = _getHash(msg.sender, _target, _value, _data, _scheduleTime);
+        require(txs[txId].status == TxStatus.None,"tx already exist");
     
-        callerTxIdTx[msg.sender][txId] = (Transaction({target: _target, value: _value, data: _data, timestamp: blockTimestamp, execute: false, cancel: false}));
+        txs[txId] = (Transaction({owner: msg.sender, target: _target, value: _value, data: _data, scheduleTime: _scheduleTime, unlockTime: _scheduleTime + delayTime, status: TxStatus.Queued}));
         callerTxIds[msg.sender].push(txId);
-        txIdCaller[txId] = msg.sender;
-        emit Queue(msg.sender, _target, txId, _value, _data, blockTimestamp);
 
-        return txId;
+        emit Queue(msg.sender, txId);
     }
 
-    function execute(bytes32 _txId) external payable returns (bytes memory) {
-        Transaction storage transaction = callerTxIdTx[msg.sender][_txId];
-        require(transaction.target != address(0),"tx not exist queued");
-        require(!transaction.execute && !transaction.cancel,"tx already exec or already cancel");
-
-        uint256 blockTimestamp = block.timestamp;
-        require(transaction.timestamp + delayTime < blockTimestamp && blockTimestamp < transaction.timestamp + execTime , "not in exec time range");
+    function execute(bytes32 _txId) external payable{
+        Transaction storage transaction = txs[_txId];
+        require(msg.sender == owner || msg.sender == transaction.owner, "access denied");
+        require(transaction.status == TxStatus.Queued,"tx status not queue");
+        require(block.timestamp >= transaction.unlockTime, "tx is lock");
 
         (bool success, bytes memory data) = transaction.target.call{value: transaction.value}(transaction.data);
         require(success,"tx exec failed");
 
-        transaction.execute = true;
-        emit Execute(msg.sender, transaction.target, _txId, transaction.value, transaction.data, blockTimestamp);
-
-        return data;
+        transaction.status = TxStatus.Executed;
+        emit Exec(msg.sender, _txId, data);
     }
 
     function cancle(bytes32 _txId) external {
-        require(msg.sender == owner, "not owner");
+        Transaction storage transaction = txs[_txId];
+        require(msg.sender == owner || msg.sender == transaction.owner, "access denied");
+        require(transaction.status == TxStatus.Queued,"tx status not queue");
 
-        Transaction storage transaction = callerTxIdTx[txIdCaller[_txId]][_txId];
-        require(transaction.target != address(0),"tx not exist queued");
-        require(!transaction.execute && !transaction.cancel,"tx already exec or already cancel");
-
-        transaction.cancel = true;
+        transaction.status = TxStatus.Canceled;
         emit Cancel(msg.sender, _txId);
     }
 
-    function getTransaction(address _caller, bytes32 _txId) external view returns (address, uint, bytes memory, uint256, bool, bool) {
-        Transaction memory transaction = callerTxIdTx[_caller][_txId];
-        require(transaction.target != address(0),"tx not exist queued");
-        return (transaction.target,transaction.value,transaction.data,transaction.timestamp,transaction.execute,transaction.cancel);
+    function getTransaction(bytes32 _txId) external view returns (Transaction memory) {
+        Transaction memory transaction = txs[_txId];
+        require(transaction.status != TxStatus.None,"tx not exist");
+        return transaction;
     }
 
     function getTxIds(address caller) external view returns (bytes32[] memory) {
@@ -84,23 +76,23 @@ contract TimeLock {
         return abi.encodePacked(bytes4(keccak256(bytes(funcSignature))), encodedArgs);
     }
 
-    function _getHash(address _target,uint256 _value,bytes calldata _data,uint256 _timestamp) private pure returns (bytes32) {
-        return keccak256(abi.encode(_target, _value, _data, _timestamp));
+    function _getHash(address _owner,address _target,uint256 _value,bytes calldata _data,uint256 _timestamp) private pure returns (bytes32) {
+        return keccak256(abi.encode(_owner,_target, _value, _data, _timestamp));
     }
 }
 
 contract TestTimeLock {
     address immutable timeLock;
 
-    uint public status = 1;
+    uint8 public num = 1;
 
     constructor(address _timeLock) {
         timeLock = _timeLock;
     }
 
-    function test() external returns (uint256, uint256) {
+    function setNum(uint8 _num) external returns (uint8) {
         require(msg.sender == timeLock,"access denied");
-        status = 2;
-        return (66, block.timestamp);
+        num = _num;
+        return _num;
     }
 }
